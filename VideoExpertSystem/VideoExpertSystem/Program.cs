7d1ad902ea2bf508cfece75355b644fe5b359ddb
jakabij <jakabistvanj@gmail.com>
2020-02-03 11:33:28 +0100

    Added solution file

---
+++ VideoExpertSystem/VideoExpertSystem/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace VideoExpertSystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
