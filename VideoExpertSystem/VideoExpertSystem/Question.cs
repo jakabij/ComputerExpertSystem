5ba916dfafebd824c06ccaacaff63ec3f4d7e38a
jakabij <jakabistvanj@gmail.com>
2020-02-03 14:24:14 +0100

    Question completed

--- VideoExpertSystem/VideoExpertSystem/Question.cs
+++ VideoExpertSystem/VideoExpertSystem/Question.cs
@@ -6,5 +6,35 @@ namespace VideoExpertSystem
 {
     class Question
     {
+        string Id { get; set; }
+        string TheQuestion { get; set; }
+        Answer answer;
+
+        public Question(string id, string theQuestion,Answer answer)
+        {
+            Id = id;
+            TheQuestion = theQuestion;
+            this.answer = answer;
+        }
+
+        public string GetId()
+        {
+            return Id;
+        }
+
+        public string GetQuestion()
+        {
+            return TheQuestion;
+        }
+
+        public Answer GetAnswer()
+        {
+            return this.answer;
+        }
+
+        public bool EvaluateAnswerByInput(string input)
+        {
+            return answer.EvaluateAnswerByInput(input);
+        }
     }
 }
