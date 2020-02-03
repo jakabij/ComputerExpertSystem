7598c84a780f18aca1dcb84ed561e8b6401784e0
MilanMolnar <54602124+MilanMolnar@users.noreply.github.com>
2020-02-03 06:29:38 -0800

    sdasd

--- VideoExpertSystem/VideoExpertSystem/Fact.cs
+++ VideoExpertSystem/VideoExpertSystem/Fact.cs
@@ -6,5 +6,40 @@ namespace VideoExpertSystem
 {
     class Fact
     {
+        public string Description { get; set; }
+        public string Id { get; set; }
+
+        public bool Value { get; set; }
+
+        public HashSet<String> SetOfId { get; set; }
+
+        public Fact(string description, string id)
+        {
+            Description = description;
+            Id = id;
+        }
+
+        public HashSet<String> GitIdHashSet()
+        {
+            return this.SetOfId;
+        }
+
+        public void SetFactValueById(String id, bool value)
+        {
+            if (id == this.Id)
+            {
+                this.Value = value;
+            }
+        }
+
+        public bool GetValueById(String Id)
+        {
+
+        }
+
+        public string GetDescription()
+        {
+            return this.Description;
+        }
     }
 }
