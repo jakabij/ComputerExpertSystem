d9c39125bb177cafe4dd4203f42421393327dba3
jakabij <jakabistvanj@gmail.com>
2020-02-03 14:16:26 +0100

    Answer class ready

--- VideoExpertSystem/VideoExpertSystem/Answer.cs
+++ VideoExpertSystem/VideoExpertSystem/Answer.cs
@@ -6,5 +6,23 @@ namespace VideoExpertSystem
 {
     class Answer
     {
+        Value value;
+
+        public bool EvaluateAnswerByInput(string input)
+        {
+            if (input.ToLower().Equals("yes"))
+                return true;
+
+            else if (input.ToLower().Equals("no"))
+                return false;
+
+            else
+                throw new Exception("Wrong input!");
+        }
+
+        public void AddValue(Value value)
+        {
+            this.value = value;
+        }
     }
 }
