aa4184f173fae24153df8149e1c4411932633f93
Adam <borbelya90@gmail.com>
2020-02-06 14:40:02 +0100

    Fact, AnswerTester

---
+++ VideoExpertSystem/VideoExpertSystems.UnitTests/AnswerTest.cs
@@ -0,0 +1,33 @@
+﻿using NUnit.Framework;
+using System;
+using VideoExpertSystem;
+
+namespace VideoExpertSystems.UnitTests
+{
+    public class AnswerTest
+    {
+        [Test]
+        public void AddValue_ValueAdded_ValueListLenghtIncrease()
+        {
+            var value = new SingleValue("", true);
+            var answer = new Answer();
+
+            answer.AddValue(value);
+            answer.AddValue(value);
+
+            Assert.AreEqual(2, answer.valueList.Count);
+        }
+        [Test]
+        public void EvaluateAnswerByInput_SeePossibleOutputs_FalseTrueException()
+        {
+            var answer = new Answer();
+
+            answer.AddValue(new SingleValue("a", true));
+            answer.AddValue(new SingleValue("b", false));
+
+            Assert.IsTrue(answer.EvaluateAnswerByInput("a"));
+            Assert.IsFalse(answer.EvaluateAnswerByInput("b"));
+            Assert.Throws<System.Exception>(() => answer.EvaluateAnswerByInput("g"));
+        }
+    }
+}
