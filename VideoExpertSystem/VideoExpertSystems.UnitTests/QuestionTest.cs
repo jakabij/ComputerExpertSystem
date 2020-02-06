bb9cd86e3b16ed46e95d4c25e39ed987a1a650da
Adam <borbelya90@gmail.com>
2020-02-06 15:18:10 +0100

    Question

---
+++ VideoExpertSystem/VideoExpertSystems.UnitTests/QuestionTest.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+using VideoExpertSystem;
+
+namespace VideoExpertSystems.UnitTests
+{
+    class QuestionTest
+    {
+        [Test]
+        public void Construct_Tester_NewQuestion()
+        {
+            var q = new Question("Tibi?", "Is any milk left, darling?", new Answer());
+
+            Assert.AreEqual(q.Id, "Tibi?");
+            Assert.AreEqual(q.TheQuestion, "Is any milk left, darling?");
+        }
+    }
+}
